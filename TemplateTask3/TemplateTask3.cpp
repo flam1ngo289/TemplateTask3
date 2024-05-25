35dc8bfbdc478df0a4ec8854a1ff8e4de1ce1e42
flam1ngo <kris.s.03@mail.ru>
2024-05-25 14:29:24 +0300

    Initial commit

---
+++ TemplateTask3/TemplateTask3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class functor {
+    int sum;
+    int count;
+public:
+    functor () : sum{0}, count{0} { }
+    void operator()(int x) {
+        if (x % 3 == 0) {
+            sum += x; 
+            ++count;
+        }
+    }
+
+    void get_sum() {
+        std::cout << "[OUT]: get_sum() = " << sum << std::endl;
+    }
+
+    void get_count() {
+        std::cout << "[OUT]: get_count() = " << count << std::endl;
+    }
+    ~functor() { }
+};
+int main()
+{
+    functor f;
+    std::vector<int> vec = { 4, 1, 3, 6, 25, 54 };
+    f = std::for_each(vec.begin(), vec.end(), f);
+
+    std::cout << "[IN]:";
+    for (int x : vec) {
+        std::cout << " " << x;
+    }
+    std::cout << std::endl;
+
+    f.get_sum();
+    f.get_count();
+
+    return 0;
+}
+
